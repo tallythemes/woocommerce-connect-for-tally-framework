11e66b491c8922dfe6483821e7c896971b23ca2f
Sazzad Hussain <wedcoder09@gmail.com>
2014-07-16 17:52:53 +0600

    improve the plugin

--- woocommerce-connect-for-tally-framework.php
+++ woocommerce-connect-for-tally-framework.php
@@ -5,7 +5,7 @@
  * Description: Add basic woocommercee templating and Style for  <strong> Tally Framework</strong>
  * Author:      TallyThemes
  * Author URI:  http://tallythemes.com/
- * Version:     1.0
+ * Version:     1.1
  * Text Domain: woocommerce_connect_for_tally
  * Domain Path: /languages/
  * Name Space: wootallyc
@@ -22,7 +22,7 @@ define('WOOTALLYC', 'Woocommerce Connect For Tally Framework' );
 define('WOOTALLYC_URL', site_url(str_replace( $path_abs, '', $path_dir )) );
 define('WOOTALLYC_DRI', $path_dir );
 define('WOOTALLYC_TEMPLATE', WOOTALLYC_DRI.'woocommerce' );
-define('WOOTALLYC_VERSION', 1.0 );
+define('WOOTALLYC_VERSION', 1.1 );
 
 
 /*
@@ -41,8 +41,9 @@ function wootallyc_load_plugin_textdomain(){
 add_action( 'after_setup_theme', 'wootallyc_init_load' );
 function wootallyc_init_load(){
     /** Fail silently if WooCommerce is not activated */
-	if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) )
-		return;	
+	if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) return;	
+		
+	if(!function_exists('tally_option')) return;	
 	
 	global $woocommerce;
 	
@@ -57,7 +58,183 @@ function wootallyc_init_load(){
 	/** Removing some unwanted element from woocommerce page content */
 	remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 	remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+	remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
 	add_filter('woocommerce_show_page_title', '__return_false');
+	remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
+	remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+	
+	/* Setup Shop sidebar*/
+	register_sidebar( array(
+		'name' => __('Woo Shop Sidebar', 'woocommerce_connect_for_tally'),
+		'id' => 'wooshop',
+		'description' => __('Woocommerce shop Sidebar Widgets', 'woocommerce_connect_for_tally'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => "</div><div class='clear' style='height:30px;'></div>",
+		'before_title' => '<h4 class="heading">',
+		'after_title' => '</h4>',
+	));
+	remove_action( 'tally_sidebar', 'tally_do_sidebar' );
+	add_action( 'tally_sidebar', 'wootallyc_add_shop_sidebar' );
+	
+	
+	/* Add Tally Framework's page metabox to the product page*/
+	add_filter('tally_ot_page_metabox', 'wootallyc_add_tally_page_metabox');
+	
+	/*Add Theme option*/
+	add_filter('option_tree_settings_args', 'wootallyc_add_theme_option');
+	
+	/*
+	 Apply filter for the sidebar layout by theme options
+	--------------------------------*/
+	add_filter('tally_sitebar_layout_option', 'wootallyc_tally_sitebar_layout_option');
+}
+
+
+
+/*
+ Adding Shop sidebar in the theme
+--------------------------------*/
+function wootallyc_add_shop_sidebar(){
+	if ( ! dynamic_sidebar( 'wooshop' ) && current_user_can( 'edit_theme_options' )  ) {
+		if(function_exists('tally_default_widget_area_content')){ tally_default_widget_area_content( __( 'WooCommerce Shop Sidebar Widget Area', 'tally_textdomain' ) ); };
+	}	
+}
+
+
+/*
+ Add Theme option
+--------------------------------*/
+function wootallyc_add_theme_option($custom_settings){
+	$custom_settings['sections'][] = array( 'id' => 'woocommerce','title' => 'WooCommerce');
+	
+	$custom_settings['settings']['woocommerce_archive_sidebar_layout'] = array(
+		'id'          => 'woocommerce_archive_sidebar_layout',
+        'label'       => __('Default Sidebar Layout for Product Archive Page', 'tally_taxdomain'),
+        'desc'        => __('This is the global sidebar layout for WooCommerce Product Archive Pages.', 'tally_taxdomain'),
+        'std'         => tally_option_std('woocommerce_archive_sidebar_layout'),
+        'type'        => 'radio-image',
+        'section'     => 'woocommerce',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'class'       => '',
+		'choices'     => array(
+			 array( 'label' => 'full-width-content', 'value' => 'full-width-content', 'src' => TALLY_URL.'/core/assets/images/admin/c.gif'),
+			 array( 'label' => 'Content - Sidebar', 'value' => 'content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/cs.gif'),
+			 array( 'label' => 'Content - Sidebar - Sidebar', 'value' => 'content-sidebar-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/css.gif'),
+			 array( 'label' => 'Sidebar - Content', 'value' => 'sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/sc.gif'),
+			 array( 'label' => 'Sidebar - Content - Sidebar', 'value' => 'sidebar-content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/scs.gif'),
+			 array( 'label' => 'Sidebar - Sidebar - Content', 'value' => 'sidebar-sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/ssc.gif'),
+		)
+	);
+	
+	
+	$custom_settings['settings']['woocommerce_cat_sidebar_layout'] = array(
+		'id'          => 'woocommerce_cat_sidebar_layout',
+        'label'       => __('Default Sidebar Layout for Product Category Archive Page', 'tally_taxdomain'),
+        'desc'        => __('This is the global sidebar layout for WooCommerce Product Category Archive Pages.', 'tally_taxdomain'),
+        'std'         => tally_option_std('woocommerce_cat_sidebar_layout'),
+        'type'        => 'radio-image',
+        'section'     => 'woocommerce',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'class'       => '',
+		'choices'     => array(
+			 array( 'label' => 'full-width-content', 'value' => 'full-width-content', 'src' => TALLY_URL.'/core/assets/images/admin/c.gif'),
+			 array( 'label' => 'Content - Sidebar', 'value' => 'content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/cs.gif'),
+			 array( 'label' => 'Content - Sidebar - Sidebar', 'value' => 'content-sidebar-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/css.gif'),
+			 array( 'label' => 'Sidebar - Content', 'value' => 'sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/sc.gif'),
+			 array( 'label' => 'Sidebar - Content - Sidebar', 'value' => 'sidebar-content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/scs.gif'),
+			 array( 'label' => 'Sidebar - Sidebar - Content', 'value' => 'sidebar-sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/ssc.gif'),
+		)
+	);
+	
+	
+	$custom_settings['settings']['woocommerce_tags_sidebar_layout'] = array(
+		'id'          => 'woocommerce_tags_sidebar_layout',
+        'label'       => __('Default Sidebar Layout for Product Tags Archive Page', 'tally_taxdomain'),
+        'desc'        => __('This is the global sidebar layout for WooCommerce Product Tags Archive Pages.', 'tally_taxdomain'),
+        'std'         => tally_option_std('woocommerce_tags_sidebar_layout'),
+        'type'        => 'radio-image',
+        'section'     => 'woocommerce',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'class'       => '',
+		'choices'     => array(
+			 array( 'label' => 'full-width-content', 'value' => 'full-width-content', 'src' => TALLY_URL.'/core/assets/images/admin/c.gif'),
+			 array( 'label' => 'Content - Sidebar', 'value' => 'content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/cs.gif'),
+			 array( 'label' => 'Content - Sidebar - Sidebar', 'value' => 'content-sidebar-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/css.gif'),
+			 array( 'label' => 'Sidebar - Content', 'value' => 'sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/sc.gif'),
+			 array( 'label' => 'Sidebar - Content - Sidebar', 'value' => 'sidebar-content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/scs.gif'),
+			 array( 'label' => 'Sidebar - Sidebar - Content', 'value' => 'sidebar-sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/ssc.gif'),
+		)
+	);
+	
+	
+	$custom_settings['settings']['woocommerce_single_sidebar_layout'] = array(
+		'id'          => 'woocommerce_single_sidebar_layout',
+        'label'       => __('Default Sidebar Layout for Single Product Page', 'tally_taxdomain'),
+        'desc'        => __('This is the global sidebar layout for WooCommerce Single Product Page', 'tally_taxdomain'),
+        'std'         => tally_option_std('woocommerce_single_sidebar_layout'),
+        'type'        => 'radio-image',
+        'section'     => 'woocommerce',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'class'       => '',
+		'choices'     => array(
+			 array( 'label' => 'full-width-content', 'value' => 'full-width-content', 'src' => TALLY_URL.'/core/assets/images/admin/c.gif'),
+			 array( 'label' => 'Content - Sidebar', 'value' => 'content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/cs.gif'),
+			 array( 'label' => 'Content - Sidebar - Sidebar', 'value' => 'content-sidebar-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/css.gif'),
+			 array( 'label' => 'Sidebar - Content', 'value' => 'sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/sc.gif'),
+			 array( 'label' => 'Sidebar - Content - Sidebar', 'value' => 'sidebar-content-sidebar', 'src' => TALLY_URL.'/core/assets/images/admin/scs.gif'),
+			 array( 'label' => 'Sidebar - Sidebar - Content', 'value' => 'sidebar-sidebar-content', 'src' => TALLY_URL.'/core/assets/images/admin/ssc.gif'),
+		)
+	);
+		
+	return $custom_settings;
+}
+
+
+/*
+ Apply filter for the sidebar layout by theme options
+--------------------------------*/
+function wootallyc_tally_sitebar_layout_option($sidebar_layout){
+	global $wp_query;
+	$custom_field = get_post_meta( get_the_ID(), 'tally_sidebar_layout', true );
+	
+	if(is_single() && 'product' == get_post_type()){
+		
+		$sidebar_layout  = $custom_field ? $custom_field : tally_option( 'woocommerce_single_sidebar_layout' );
+		
+	}elseif(is_post_type_archive( 'product' ) ||  is_page( get_option( 'woocommerce_shop_page_id' ) )){
+		
+		$sidebar_layout  = $custom_field ? $custom_field : tally_option( 'woocommerce_archive_sidebar_layout' );
+		
+	}elseif(is_tax('product_tag')){
+		
+		$sidebar_layout  = $custom_field ? $custom_field : tally_option( 'woocommerce_tags_sidebar_layout' );
+		
+	}elseif(is_tax('product_cat')){
+		
+		$sidebar_layout  = $custom_field ? $custom_field : tally_option( 'woocommerce_cat_sidebar_layout' );
+	}
+	
+	return $sidebar_layout;
+}
+
+
+/*
+ Add Tally Framework's page metabox to the product page
+--------------------------------*/
+function wootallyc_add_tally_page_metabox($post){
+	if(post_type_exists('product')){
+		$post[] = 'product';
+	}
+	
+	return $post;
 }
 
 
